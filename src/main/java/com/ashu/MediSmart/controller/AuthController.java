ffed403a09f3ae4d97a0ef661fbadb87d4428968
RajatPandey10 <pandeyrajat9971@gmail.com>
2025-09-04 23:04:30 +0530

    Added role-specific registration endpoints in AuthController

--- src/main/java/com/ashu/MediSmart/controller/AuthController.java
+++ src/main/java/com/ashu/MediSmart/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.ashu.MediSmart.DTO.UserDTO;
 import com.ashu.MediSmart.entity.User;
 import com.ashu.MediSmart.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,4 +19,25 @@ public class AuthController {
     public User registerUser(@RequestBody UserDTO userDTO) {
         return userService.registerUser(userDTO);
     }
+
+    @PostMapping("/register/patient")
+    public ResponseEntity<User> registerPatient(@RequestBody UserDTO userDTO) {
+        userDTO.setRole("PATIENT");
+        return ResponseEntity.ok(userService.registerUser(userDTO));
+    }
+
+    @PostMapping("/register/doctor")
+    public ResponseEntity<User> registerDoctor(@RequestBody UserDTO userDTO) {
+        userDTO.setRole("DOCTOR");
+        return ResponseEntity.ok(userService.registerUser(userDTO));
+    }
+
+    @PostMapping("/register/admin")
+    public ResponseEntity<User> registerAdmin(@RequestBody UserDTO userDTO) {
+        userDTO.setRole("ADMIN");
+        return ResponseEntity.ok(userService.registerUser(userDTO));
+    }
+
+
+
 }
